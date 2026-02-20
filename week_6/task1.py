d81860b3fb3f724a6bd27113ad4e2e378db3e3e4
Siddhesh M <SBMPC.Student@SVKMGRP.COM>
2026-02-20 09:40:34 +0530

    template created

---
+++ week_6/task1.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    bank_name = "National Bank"
+    total_accounts = 0
+    total_bank_balance = 0
+
+    def __init__(self,account_holder):
+        self.account_holder = account_holder
+        self.balance = 0
+    
+    def deposit(self,amount): 
+        pass
+    
+    def withdraw(self, amount):
+        pass    
+    
+    def display_account_info(self):
+        pass
+    
